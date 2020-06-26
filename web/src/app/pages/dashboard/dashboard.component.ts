4ea46d98d9cf4b2673823f50546927f8da5d818f
willdavsmith <willdavsmith@gmail.com>
2020-06-26 09:38:12 -0700

    added card on main overview to show control plane healthiness

--- web/src/app/pages/dashboard/dashboard.component.ts
+++ web/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { OnInit, Component, OnDestroy } from '@angular/core';
 import { InstanceService } from '../../instances/instance.service';
+import { StatusService } from 'src/app/status/status.service';
 
 @Component({
   selector: 'ngx-dashboard',
@@ -10,9 +11,27 @@ import { InstanceService } from '../../instances/instance.service';
 export class DashboardComponent implements OnInit, OnDestroy {
   public data: any[];
   public displayedColumns: string[] = ['name', 'status', 'age'];
+  public controlPlaneHealthiness: number;
   private intervalHandler;
 
-  constructor(private instanceService: InstanceService) { }
+  constructor(
+    private instanceService: InstanceService,
+    private statusService: StatusService
+  ) { }
+
+  ngOnInit() {
+    this.getInstances();
+    this.getControlPlaneHealthiness();
+    
+    this.intervalHandler = setInterval(() => { 
+      this.getInstances();
+      this.getControlPlaneHealthiness();
+    }, 3000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalHandler);
+  }
 
   getInstances() {
     this.instanceService.getInstances().subscribe((data: any[]) => {
@@ -20,12 +39,15 @@ export class DashboardComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit() {
-    this.getInstances();
-    this.intervalHandler = setInterval(() => { this.getInstances(); }, 3000);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.intervalHandler);
+  getControlPlaneHealthiness(): void {
+    var totalHealthy = 0;
+    this.statusService.getControlPlaneStatus().subscribe((data: any[]) => {
+      data.forEach(service => {
+        if (service.Healthy == 'True') {
+          totalHealthy += 1;
+        }
+      });
+      this.controlPlaneHealthiness = totalHealthy;
+    });
   }
 }
--- web/src/app/pages/dashboard/dashboard.module.ts
+++ web/src/app/pages/dashboard/dashboard.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     RouterModule,
+    CommonModule,
     MatTableModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   declarations: [
     DashboardComponent,
