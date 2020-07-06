79a6e424b0d9ce1b0a94af4bd7c91ed9ec233949
willdavsmith <willdavsmith@gmail.com>
2020-07-06 16:04:32 -0700

    fixed issue where reloading causes timeout

--- cmd/webserver.go
+++ cmd/webserver.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	components "github.com/dapr/dashboard/pkg/components"
@@ -16,7 +19,6 @@ import (
 )
 
 const (
-	dir  = "./web/dist"
 	port = 8080
 )
 
@@ -36,6 +38,15 @@ var etagHeaders = []string{
 	"If-None-Match",
 	"If-Range",
 	"If-Unmodified-Since",
+}
+
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
 }
 
 var inst instances.Instances
@@ -64,16 +75,63 @@ func RunWebServer() {
 	r.HandleFunc("/api/environments", getEnvironmentsHandler)
 	r.HandleFunc("/api/controlplanestatus", getControlPlaneHandler)
 
-	fs := http.FileServer(http.Dir(dir))
-	r.HandleFunc("/{rest:.*}", angularHandler)
-	r.PathPrefix("/").Handler(noCache(http.StripPrefix("/", fs)))
+	spa := spaHandler{staticPath: "web/dist", indexPath: "index.html"}
+
+	r.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
 	fmt.Println(fmt.Sprintf("Dapr Dashboard running on http://localhost:%v", port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), r))
+	log.Fatal(srv.ListenAndServe())
 }
 
-func angularHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./web/dist/index.html")
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// get the volume of the absolute path and remove it
+	volume := filepath.VolumeName(path)
+	path = strings.Replace(path, volume, "", 1)
+
+	// if the path is not a static file or library dependency (i.e. .js file)
+	// serve it directly without prepending its path
+	// if !(strings.Contains(path, "assets") || strings.Contains(path, "lib")) {
+	// 	path = filepath.Base(path)
+	// }
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func getInstancesHandler(w http.ResponseWriter, r *http.Request) {
