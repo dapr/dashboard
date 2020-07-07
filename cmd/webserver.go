34fc40ede86219c7d33f01d8de5d4d4e94fd6c79
Will Smith <willdavsmith@gmail.com>
2020-07-06 17:40:35 -0700

    Standalone bugfix and refresh functionality (#44)
    * fixed issue where MIME types were unknown
    
    * added routing for web server to allow refresh
    
    * fixed issue where reloading causes timeout
    
    * added api subrouter, moved favicon location

--- cmd/webserver.go
+++ cmd/webserver.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	components "github.com/dapr/dashboard/pkg/components"
@@ -16,7 +19,7 @@ import (
 )
 
 const (
-	dir = "./web/dist"
+	port = 8080
 )
 
 var epoch = time.Unix(0, 0).Format(time.RFC1123)
@@ -37,12 +40,19 @@ var etagHeaders = []string{
 	"If-Unmodified-Since",
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
 var inst instances.Instances
 var comps components.Components
 var configs configurations.Configurations
 var stats status.Status
-
-const port = 8080
 
 // RunWebServer starts the web server that serves the Dapr UI dashboard and the API
 func RunWebServer() {
@@ -53,21 +63,71 @@ func RunWebServer() {
 	stats = status.NewStatus(kubeClient)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/api/features", getFeaturesHandler)
-	r.HandleFunc("/api/instances", getInstancesHandler)
-	r.HandleFunc("/api/instances/{id}", deleteInstancesHandler).Methods("DELETE")
-	r.HandleFunc("/api/instances/{id}", getInstanceHandler).Methods("GET")
-	r.HandleFunc("/api/instances/{id}/logs", getLogsHandler)
-	r.HandleFunc("/api/components", getComponentsHandler)
-	r.HandleFunc("/api/components/status", getComponentsStatusHandler)
-	r.HandleFunc("/api/configuration/{id}", getConfigurationHandler)
-	r.HandleFunc("/api/daprconfig", getDaprConfigHandler)
-	r.HandleFunc("/api/environments", getEnvironmentsHandler)
-	r.HandleFunc("/api/controlplanestatus", getControlPlaneHandler)
-	r.PathPrefix("/").Handler(noCache(http.StripPrefix("/", http.FileServer(http.Dir(dir)))))
+
+	api := r.PathPrefix("/api/").Subrouter()
+	api.HandleFunc("/features", getFeaturesHandler).Methods("GET")
+	api.HandleFunc("/instances", getInstancesHandler).Methods("GET")
+	api.HandleFunc("/instances/{id}", deleteInstancesHandler).Methods("DELETE")
+	api.HandleFunc("/instances/{id}", getInstanceHandler).Methods("GET")
+	api.HandleFunc("/instances/{id}/logs", getLogsHandler).Methods("GET")
+	api.HandleFunc("/components", getComponentsHandler).Methods("GET")
+	api.HandleFunc("/components/status", getComponentsStatusHandler).Methods("GET")
+	api.HandleFunc("/configuration/{id}", getConfigurationHandler).Methods("GET")
+	api.HandleFunc("/daprconfig", getDaprConfigHandler).Methods("GET")
+	api.HandleFunc("/environments", getEnvironmentsHandler).Methods("GET")
+	api.HandleFunc("/controlplanestatus", getControlPlaneHandler).Methods("GET")
+
+	spa := spaHandler{staticPath: "web/dist", indexPath: "index.html"}
+
+	r.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf("127.0.0.1:%v", port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
 	fmt.Println(fmt.Sprintf("Dapr Dashboard running on http://localhost:%v", port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), r))
+	log.Fatal(srv.ListenAndServe())
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// get the volume of the absolute path and remove it
+	volume := filepath.VolumeName(path)
+	path = strings.Replace(path, volume, "", 1)
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func getInstancesHandler(w http.ResponseWriter, r *http.Request) {
