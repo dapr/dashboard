b3741362a6670162b5faa07d26e4a5ff5fe1b275
Aman Bhardwaj <amanbha@microsoft.com>
2020-08-01 17:52:12 +0000

    generate a .zip as well for windows.

--- build.sh
+++ build.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 # build node angular code
+
+# --generate-artifacts flag is passed in github actions workflow to generate artifacts.
+GENERATE_ARTIFACTS=false
+RELEASE_DIR=./release
+OTHER_ARGUMENTS=()
+
+for arg in "$@"
+do
+    case $arg in
+        -a|--generate-artifacts)
+        GENERATE_ARTIFACTS=true
+        shift # Remove ----generate-artifacts from processing
+        ;;
+        -r|--release-dir)
+        RELEASE_DIR="$2"
+        shift # Remove argument name from processing
+        shift # Remove argument value from processing
+        ;;
+        *)
+        OTHER_ARGUMENTS+=("$1")
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
+echo "# Release directory: $RELEASE_DIR"
+echo "# Generate artifacts: $GENERATE_ARTIFACTS"
+#echo "# Other arguments: ${OTHER_ARGUMENTS[*]}"
+
 cd web
 npm i
 ng build --base-href ./
 cd ..
-release_dir=./release
-artifacts_dir=${release_dir}/artifacts
-rm -r -f ${release_dir}
-mkdir -p ${artifacts_dir}
+rm -r -f ${RELEASE_DIR}
+mkdir -p ${RELEASE_DIR}
 
 
 # build go code and prepare release dir for platforms.
@@ -27,14 +54,29 @@ do
   echo building go executable for $GOOS $GOARCH, output will be $go_executable_output_file
   env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -a -o $go_executable_output_file
 
-  platform_release_dir=${release_dir}/${platform}
-  platform_artifact_archive=${artifacts_dir}/dashboard_${platform}
+  platform_release_dir=${RELEASE_DIR}/${platform}  
 
   echo preparing release dir ${platform_release_dir}
   mkdir -p ${platform_release_dir}/web/
   cp -r ./web/dist ${platform_release_dir}/web/
   mv ./$go_executable_output_file ${platform_release_dir}
 
-  # create archives
-  tar -zcf ${platform_artifact_archive}.tar.gz ${platform_release_dir}
+  # create archives if specified
+  if [ $GENERATE_ARTIFACTS = "true" ]; then      
+    artifacts_dir=${RELEASE_DIR}/artifacts
+    mkdir -p ${artifacts_dir}
+    platform_artifact_archive=${artifacts_dir}/dashboard_${platform}
+    echo generating artifacts in ${artifacts_dir}
+    
+    tar -zcf ${platform_artifact_archive}.tar.gz ${platform_release_dir}
+    
+    # for windows generate .zip also (needs zip to be installed)
+    if [ $GOOS = "windows" ]; then     
+      if hash zip 2>/dev/null; then
+        zip -r -q ${platform_artifact_archive}.zip ${platform_release_dir}
+      else
+       echo skipping generation of ${platform_artifact_archive}.zip as zip is not installed. 
+      fi  
+    fi
+  fi
 done
