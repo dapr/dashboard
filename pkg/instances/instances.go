069c1250eb235d24865e01b9609228c212b95ae0
Mukundan Sundararajan <musundar@microsoft.com>
2020-11-23 10:06:40 -0800

    add dapr-placement-server to control plane labels add tests. run go mod tidy.

--- pkg/instances/instances.go
+++ pkg/instances/instances.go
@@ -30,7 +30,8 @@ const (
 )
 
 var (
-	controlPlaneLabels = [...]string{"dapr-operator", "dapr-sentry", "dapr-placement", "dapr-sidecar-injector"}
+	//TODO: dapr-placement left for backward compatability, can be removed after v1.0 release
+	controlPlaneLabels = [...]string{"dapr-operator", "dapr-sentry", "dapr-placement", "dapr-placement-server", "dapr-sidecar-injector"}
 )
 
 // Instances is an interface to interact with running Dapr instances in Kubernetes or Standalone modes
@@ -50,7 +51,7 @@ type Instances interface {
 
 type instances struct {
 	platform       string
-	kubeClient     *kubernetes.Clientset
+	kubeClient     kubernetes.Interface
 	getInstancesFn func(string) []Instance
 	getScopesFn    func() []string
 }
@@ -264,9 +265,8 @@ func (i *instances) GetControlPlaneStatus() []StatusOutput {
 				options.LabelSelector = labels.FormatLabels(labelSelector)
 
 				p, err := i.kubeClient.CoreV1().Pods(v1.NamespaceAll).List(options)
-				if err == nil && len(p.Items) == 1 {
+				if err == nil && len(p.Items) != 0 {
 					pod := p.Items[0]
-
 					image := pod.Spec.Containers[0].Image
 					namespace := pod.GetNamespace()
 					age := age.GetAge(pod.CreationTimestamp.Time)
--- pkg/instances/instances_test.go
+++ pkg/instances/instances_test.go
@@ -3,3 +3,91 @@
 // Licensed under the MIT License.
 // ------------------------------------------------------------
 package instances
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func newTestSimpleK8s(objects ...runtime.Object) instances {
+	client := instances{}
+	client.kubeClient = fake.NewSimpleClientset(objects...)
+	return client
+}
+
+func newDaprControlPlanePod(name string, appName string, creationTime time.Time, state v1.ContainerState, ready bool) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   "dapr-system",
+			Annotations: map[string]string{},
+			Labels: map[string]string{
+				"app": appName,
+			},
+			CreationTimestamp: metav1.Time{
+				Time: creationTime,
+			},
+		},
+		Status: v1.PodStatus{
+			ContainerStatuses: []v1.ContainerStatus{
+				{
+					State: state,
+					Ready: ready,
+				},
+			},
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Image: name + ":0.0.1",
+				},
+			},
+		},
+	}
+}
+
+func TestControlPlaneServices(t *testing.T) {
+	controlPlaneServices := []struct {
+		name    string
+		appName string
+	}{
+		{"dapr-operator-67d7d7bb6c-7h96c", "dapr-operator"},
+		{"dapr-operator-67d7d7bb6c-2h96d", "dapr-operator"},
+		{"dapr-operator-67d7d7bb6c-3h96c", "dapr-operator"},
+		{"dapr-placement-server-0", "dapr-placement-server"},
+		{"dapr-placement-server-1", "dapr-placement-server"},
+		{"dapr-placement-server-2", "dapr-placement-server"},
+		{"dapr-sentry-647759cd46-9ptks", "dapr-sentry"},
+		{"dapr-sentry-647759cd46-aptks", "dapr-sentry"},
+		{"dapr-sentry-647759cd46-bptks", "dapr-sentry"},
+		{"dapr-sidecar-injector-74648c9dcb-5bsmn", "dapr-sidecar-injector"},
+		{"dapr-sidecar-injector-74648c9dcb-6bsmn", "dapr-sidecar-injector"},
+		{"dapr-sidecar-injector-74648c9dcb-7bsmn", "dapr-sidecar-injector"},
+	}
+
+	runtimeObj := make([]runtime.Object, len(controlPlaneServices))
+	for i, s := range controlPlaneServices {
+		testTime := time.Now()
+		runtimeObj[i] = newDaprControlPlanePod(
+			s.name, s.appName,
+			testTime.Add(time.Duration(-20)*time.Minute),
+			v1.ContainerState{
+				Running: &v1.ContainerStateRunning{
+					StartedAt: metav1.Time{
+						Time: testTime.Add(time.Duration(-19) * time.Minute),
+					},
+				},
+			}, true)
+	}
+
+	k8s := newTestSimpleK8s(runtimeObj...)
+	status := k8s.GetControlPlaneStatus()
+
+	assert.Equal(t, 4, len(status), "Expected status list length to match")
+}
