59ddab3aefb96e7f67b6e20afe24f75dc7e9ffdc
Lorenzo Montanari <l0ll098@users.noreply.github.com>
2022-02-15 20:19:55 +0100

    With a cast, it should work (or at least locally it does)
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>

--- web/src/app/pages/pages.component.ts
+++ web/src/app/pages/pages.component.ts
@@ -136,7 +136,7 @@ export class AboutDialogComponent {
   @ViewChild('info', { static: true }) public info!: ElementRef;
   public version = VERSION;
 
-  dashboardVersion = this.version.semver?.version || this.version.version;
+  dashboardVersion = (this.version.semver as any)?.version || this.version.version;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
