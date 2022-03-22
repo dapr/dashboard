12e3d2a08623fe77955865d0150a59eb85ab16cb
Lorenzo Montanari <l0ll098@users.noreply.github.com>
2022-03-22 04:29:18 +0100

    Several Dashboard improvements (#190)
    * Added Angular proxy configuration
    
    This helps in development mode
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Removed unused references
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Minor improvements to the editor component
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Minor improvements to the editor component
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Editor component will now fill the entire screen
    
    This is especially useful with longer configuration files
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Setting a dynamic height allows for a smoother transition between tabs
    
    This also fixes an issue that resulted in a vertical scrollbar being showed
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed blurry bold labels
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed "Labels" and "Selector" Overview page
    
    When in a K8S cluster, the overview page shows two more columns, "Labels" and "Selector".
    Previously they showed the string "app:" if no matching labels were found.
    Now, if a deployment has appropriate labels, we will show them.
    Otherwise dashboard UI falls back to "-"
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed an issue that caused page to break while switching between "Actors" and "Logs" tabs
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Changed icons in "Components" page
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Added a "zero state" message in overview page.
    
    If there are no running applications, we show an helper message that guides user to the documentation page
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed an issue that resulted in wrong dapr dashboard version being showed in the "info" popup
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Revert "Fixed an issue that resulted in wrong dapr dashboard version being showed in the "info" popup"
    
    This reverts commit c628d5e5e8eebf1abdc714276f25d5460df15827.
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed an issue that resulted in wrong dapr dashboard version being showed in the "info" popup
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Fixed a (false positive) possibly null reference error
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * Apparently 'semver.version' is not always defined
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>
    
    * With a cast, it should work (or at least locally it does)
    
    Signed-off-by: Lorenzo Montanari <l0ll098@users.noreply.github.com>

--- web/src/app/pages/dapr-components/dapr-components.module.ts
+++ web/src/app/pages/dapr-components/dapr-components.module.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { DaprComponentsComponent } from './dapr-components.component';
-import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     MatTableModule,
+    MatIconModule
   ],
   declarations: [
     DaprComponentsComponent
